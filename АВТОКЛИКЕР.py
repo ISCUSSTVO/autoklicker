d29b0a185124518b728b50a4f5fd52905174b3dc
ISCUSSTVO <nikishka228@list.ru>
2024-06-28 17:57:41 +0700

    Create АВТОКЛИКЕР.py

---
+++ АВТОКЛИКЕР.py
@@ -0,0 +1,13 @@
+import keyboard
+import mouse
+import time
+work = False
+def change():
+    global work
+    work = not work
+work =False
+keyboard.add_hotkey('`', change)
+while True:
+    if work:
+        mouse.click(button = 'left')
+        time.sleep(0.00001)
